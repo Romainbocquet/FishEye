3badf538519d4b99ed55cec815fd1f6c23bf07d2
Romainbocquet <94678672+Romainbocquet@users.noreply.github.com>
2023-01-11 11:38:38 +0100

    base

---
+++ js/App.js
@@ -0,0 +1,29 @@
+class App {
+    constructor() {
+        this.$moviesWrapper = document.querySelector('.movies-wrapper')
+        
+        this.moviesApi = new MovieApi('/data/new-movie-data.json')
+        this.externalMoviesApi = new MovieApi('/data/external-movie-data.json')
+    }
+
+    async main() {
+        const moviesData = await this.moviesApi.get()
+        const externalMoviesData = await this.externalMoviesApi.get()
+
+
+        const Movies = moviesData.map(movie => new MoviesFactory(movie, 'newApi'))
+        const ExternalMovies = externalMoviesData.map(movie => new MoviesFactory(movie, 'externalApi'))
+
+        const FullMovies = Movies.concat(ExternalMovies)
+
+        FullMovies.forEach(movie => {
+                const Template = new MovieCard(movie)
+                this.$moviesWrapper.appendChild(
+                    Template.createMovieCard()
+                )
+        })
+    }
+}
+
+const app = new App()
+app.main()
